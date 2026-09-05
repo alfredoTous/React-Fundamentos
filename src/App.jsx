0607d48233065c00a59a6b4e4c1d2ef5c387cc3c
alfredoTous <adtous@uninorte.edu.co>
2026-09-05 14:12:53 -0500

    feat: implement Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import CoursesSection from "./components/CoursesSection";
 import CounterSection from "./components/CounterSection";
+import Footer from "./components/Footer"
 import './App.css'
 
 function App()
@@ -9,9 +10,12 @@ function App()
     return (
         <>
             <Header/>
+
             <Banner/>
             <CoursesSection/>
             <CounterSection/>
+
+            <Footer/>
         </>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+function Footer()
+{
+    return (
+        <div className="footer">
+            <p>
+                © 2026 <a href="#Inicio">ReactAcademy</a>. Taller02 -- React Fundamentos.
+            </p>
+        </div>
+    )
+}
+
+
+export default Footer;
